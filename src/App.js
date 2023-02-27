f9457cf4f5afa43c8cb1c34f6de41c74aaf9e36a
Sovlus <kubbaz122@gmail.com>
2023-02-27 23:45:55 +0100

    finished .login

--- src/App.js
+++ src/App.js
@@ -1,40 +1,12 @@
-import React, { useState } from 'react';
-import firebase from './firebase';
+import React from 'react';
+import Login from './Login';
 
-const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+const App = () => {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+};
 
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      firebase.auth().signInWithEmailAndPassword(email, password)
-      .then((userCredential) => {
-        const user = userCredential.user;
-        console.log(user);
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(errorCode, errorMessage);
-      });
-    };
-
-      return (
-        <div>
-          <h2>Login</h2>
-          <form onSubmit={handleSubmit}>
-            <div>
-              <label>Email</label>
-              <input type="email" value={email} onChange={(e) => setEmail(e.targer.value)} />
-            </div>
-            <div>
-              <label>Password</label>
-              <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-            </div>
-            <button type="submit">Login</button>
-          </form>
-        </div>
-      )};
-
-
-export default Login;
+export default App;
