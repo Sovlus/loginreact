c3516bafb7dd8be360580913c8e95fd2549c4732
Sovlus <kubbaz122@gmail.com>
2023-02-27 23:41:04 +0100

    finished "Login"

--- src/App.js
+++ src/App.js
@@ -19,7 +19,22 @@ const Login = () => {
       });
     };
 
+      return (
+        <div>
+          <h2>Login</h2>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label>Email</label>
+              <input type="email" value={email} onChange={(e) => setEmail(e.targer.value)} />
+            </div>
+            <div>
+              <label>Password</label>
+              <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            </div>
+            <button type="submit">Login</button>
+          </form>
+        </div>
+      )};
 
 
-
-}
+export default Login;
