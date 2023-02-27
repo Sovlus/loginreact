e9bf9e04980318db16eda0e3653b30eff7439a6f
Sovlus <kubbaz122@gmail.com>
2023-02-27 23:36:21 +0100

    added error messages

--- src/App.js
+++ src/App.js
@@ -12,7 +12,12 @@ const Login = () => {
         const user = userCredential.user;
         console.log(user);
       })
-    }
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+      });
+    };
 
 
 
