7e57320a5ca373f6a399097f1105df7b4d553439
Sovlus <kubbaz122@gmail.com>
2023-02-28 23:59:08 +0100

    styled login.js

--- src/Login.js
+++ src/Login.js
@@ -1,15 +1,15 @@
-import React, { useState , useCallback } from 'react';
-import App from './App';
+import React, { useState, useCallback } from 'react';
 import firebase from './firebase';
 import SignUp from './signup';
-
+import App from './App';
+import './login.css';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [user, setUser] = useState(null);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = useCallback((e) => {
     e.preventDefault();
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then((userCredential) => {
@@ -19,10 +19,6 @@ const Login = () => {
         setPassword('');
       })
 
-      firebase.database().ref('users/' + user.uid).set({
-        email: email,
-        password: password
-      })
       .then(() => {
         console.log('Dane wysłane pomyślnie!');
       })
@@ -40,24 +36,24 @@ const Login = () => {
         const errorMessage = error.message;
         console.log(errorCode, errorMessage);
       });
-    };
+    }, [email, password, user]);
 
-      return (
-        <div>
-          <h2>Login</h2>
-          <form onSubmit={handleSubmit}>
-            <div>
-              <label>Email</label><br></br>
-              <input type="email" value={email} onChange={(e) => setEmail(e.target.value)}  placeholder="Email" />
-            </div>
-            <div>
-              <label>Password</label><br></br>
-              <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
-            </div>
-            <button type="submit">Login</button>
-          </form>
+  return (
+    <div className="login-container">
+      <h2 className="login-heading">Login</h2>
+      <form className="login-form" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label className="form-label">Email</label>
+          <input className="form-input" type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
         </div>
-      )};
-
+        <div className="form-group">
+          <label className="form-label">Password</label>
+          <input className="form-input" type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+        </div>
+        <button className="login-button" type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
